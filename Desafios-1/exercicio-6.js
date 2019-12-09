ccf4d10e8e55358d1a8f035460559ce42a60099f
Rogerio <rogerio_d3@me.com>
2019-12-09 10:17:28 -0300

    2

--- Desafios-1/exercicio-6.js
+++
@@ -1,5 +0,0 @@
-const usuario = 'Diego';
-const idade = 23;
-console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
-
-console.log(`O usuario ${usuario} possui ${idade} anos`);
--- Desafios-1/exercicio-7.js
+++
@@ -1,8 +0,0 @@
-const nome = 'Diego';
-const idade = 23;
-
-const usuario = {
-  nome,
-  idade,
-  cidade: 'Rio do Sul'
-};
