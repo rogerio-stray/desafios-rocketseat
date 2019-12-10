5ec49c0da8c98a78b81cd2453f115ae0f5455c35
Rogerio <rogerio_d3@me.com>
2019-12-10 15:32:34 -0300

    Exercicios webpack

--- desafio-1/main.js
+++
@@ -1,6 +0,0 @@
-
-class Teste{
-  metodo(){
-    
-  }
-}
---
+++ desafio-1/modulo-2/exercicio-1.js
@@ -0,0 +1,5 @@
+import {usuario as ClasseUsuario, idade as IdadeUsuario} from './modulo-2/functions';
+
+ClasseUsuario.info();
+console.log(ClasseUsuario);
+console.log(IdadeUsuario);
